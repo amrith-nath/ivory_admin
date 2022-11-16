88b419abd086b93685c3b65e730447978a271af9
amrith-nath <amrithnath@outlook.com>
2022-11-16 20:29:49 +0530

    new branch commit

--- lib/controllers/coupen_controller.dart
+++ lib/controllers/coupen_controller.dart
@@ -1,3 +1,20 @@
 import 'package:get/get.dart';
+import 'package:ivory_admin/models/coupen_model.dart';
+import 'package:ivory_admin/services/database_services/database_service.dart';
 
-class CoupenController extends GetxController {}
+class CoupenController extends GetxController {
+  DatabaseService databaseService = DatabaseService();
+
+  var newCoupen = {}.obs;
+
+  var coupens = <Coupen>[].obs;
+
+  @override
+  void onInit() {
+    coupens.bindStream(databaseService.getCoupens());
+    super.onInit();
+  }
+
+  get code => newCoupen['code'];
+  get amount => newCoupen['amount'];
+}
